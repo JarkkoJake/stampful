2ab657389ba9230355eae03496f4701a5f98762e
Jarkko Heinonen <71126476+JarkkoJake@users.noreply.github.com>
2022-01-19 15:26:19 +0200

    image backend
    express-upload installed, non tested initial support for thumbnail upload

--- backend/controllers/postAuctionController.js
+++ backend/controllers/postAuctionController.js
@@ -9,6 +9,17 @@ exports.postNewAuction = async (req, res) => {
   // create new auction object based on model
   var auction = new Auction.Auction(req.body);
 
+  if (req.files) {
+    console.log("files realized");
+    req.files.array.forEach(file => {
+      console.log(file.name);
+    });
+    let file = req.files.thumbnail;
+    let filepath = "images/" + file.name;
+    auction.thumbnail = filepath;
+    await file.mv(__dirname + "/../public/images/" + file.name);
+}
+
   // this function checks if the auction somehow got country, seller or category id's
   // that dont exist at the moment
   if (await auction.invalidCategories()) {
