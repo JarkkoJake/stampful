b310e23905cc4014ce172e0c149cc7623932d7ce
Steve Hommy <steve96hommy@gmail.com>
2022-02-21 13:44:04 +0200

    Detailed view

--- personaldatabase/src/components/Detailed/Detailed.js
+++ personaldatabase/src/components/Detailed/Detailed.js
@@ -6,55 +6,17 @@ import { Row, Col, Button } from "antd";
 import "antd/dist/antd.css";
 import { LeftOutlined, RightOutlined, DoubleLeftOutlined, DoubleRightOutlined, ArrowLeftOutlined } from "@ant-design/icons";
 import logo from "../../public/images/StampLogo.png";
-import "./Browse.css";
+import "./Detailed.css";
 
-const Browse = () => {
+const Detailed = () => {
   const { setRoute } = useContext(RouteContext);
 
   const [auctionItemsList, setAuctionItemsList] = useState([]);
-  const pageNumber = useRef(1);
-  const maxPageNumber = useRef(1);
-
-  const URL = constants.URL;
-
-  useEffect(() => {
-    axios
-      .get(`${URL}/auctions/listbrowse?page=${pageNumber.current}&orderBy=id`)
-      .then((res) => {
-        maxPageNumber.current = res.data.info.totalPages;
-        setAuctionItemsList(res.data.auctions);
-      });
-  },[]);
-
-  const changePage = (direction) => {
-    if ((direction < 0 && pageNumber.current === 1) || (pageNumber.current === maxPageNumber.current && direction > 0)) {
-      null;
-    } else {
-      axios
-        .get(`${URL}/auctions/listbrowse?page=${pageNumber.current + direction}&orderBy=id`)
-        .then((res) => {
-          pageNumber.current = pageNumber.current + 1 * direction;
-          maxPageNumber.current = res.data.info.totalPages;
-          setAuctionItemsList(res.data.auctions);
-          
-        });
-    }
-  };
-
-  const directPageChange = (number) => {
-    if (number === 0) number = maxPageNumber.current;
-    axios
-      .get(`${URL}/auctions/listbrowse?page=${number}&orderBy=id`)
-      .then((res) => {
-        pageNumber.current = number;
-        maxPageNumber.current = res.data.info.totalPages;
-        setAuctionItemsList(res.data.auctions);
-      });
-  };
 
   const auctionItems = auctionItemsList.map((auction) => {
-    return (
-      <Row className="auctionItem" onClick={() => setRoute("Detailed")} key={auction.id}>
+
+    return(
+      <Row className="auctionItem" onClick={() => console.log(auction)} key={auction.id}>
         <Col className="image">
           <img className="auctionImage" src={logo} alt="Logo"></img>
         </Col>
@@ -76,7 +38,7 @@ const Browse = () => {
               <h3 className="infoHeader">{auction.startingPrice  ? auction.startingPrice : "-" }{auction.currency ? auction.currency : "" }</h3>
             </div>
           </Row>
-          
+            
           <Row className="sellingPrice">
             <div className="centeringWrapper">
               <h3 className="infoHeader">{auction.sellingPrice ? auction.sellingPrice : "-" }{auction.currency ? auction.currency : "" }</h3>
@@ -102,39 +64,20 @@ const Browse = () => {
   return (
     <div>
       <div id="browseHeader">
-        <button id="back" onClick={() => setRoute("Menu")} style={{float: "left"}}>
+        <button id="back" onClick={() => setRoute("Browse")} style={{float: "left"}}>
           <ArrowLeftOutlined style={{fontSize: "2.3vh", paddingRight: "6px" }}/>
-          Back
+            Browse
         </button>
         <div id="browseView">
-          <p id="browseText"> Browse view </p>
-          
+          <p id="browseText"> Detailed view </p>
+
         </div>
       </div>
 
       <div id="browseMainBody">
         {auctionItems}
       </div>
-      <div className="paginationButtons">
-        <div className="allLeft" onClick={() => directPageChange(1)}>
-          <DoubleLeftOutlined style={{fontSize: "14px"}}/>
-        </div>
-        <div className="left">
-          <LeftOutlined onClick={() => changePage(-1)} style={{fontSize: "14px"}}/>
-        </div>
-        <div className="pageNumers">
-          {pageNumber.current}/{maxPageNumber.current}
-        </div>
-        <div className="right">
-          <RightOutlined onClick={() => changePage(1)} style={{fontSize: "14px"}}/>
-        </div>
-        <div className="allRight">
-          <DoubleRightOutlined onClick={() => directPageChange(0)} style={{fontSize: "14px"}}/>
-        </div>
-      </div>
     </div>
   );
-    
 };
-
-export default Browse;
+export default Detailed;
