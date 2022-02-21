634bbbc56638609c92c9a1ea32c63b4b97bff3fc
Jarkko Heinonen <71126476+JarkkoJake@users.noreply.github.com>
2022-02-21 11:48:17 +0200

    descriptive comments added

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -6,21 +6,33 @@ exports.getImageWithId = async (req, res) => {
   console.log(req.params.id);
 };
 
+// Post image to database, add reference to auction item in database and
+// save image file to server. Currently (21.2.2022) only supports one image (thumbnail).
 exports.postImage = async (req, res) => {
   try {
     let newImage = {auctionId: req.params.auctionId};
     let imageId = await imageDb.postImage(newImage);
     let file = req.files["thumbnail"];
+
+    // id of the created image entry to database is attached to the end
+    // of file name before extension to prevern duplicate names from
+    // causing errors
     let splitFileName = file.name.split(".");
     file.name = splitFileName[0] + "_" + imageId[0] + "." + splitFileName[1];
+
+    // build image and auction information
     newImage.id = imageId[0];
     newImage.path = "images/" + file.name;
     let auction = {
       id: req.params.auctionId,
       thumbnail: imageId
     }
+
+    // update items in database
     await auctionDb.editAuction(auction);
     await imageDb.addPath(newImage);
+
+    // save file
     await file.mv(join(__dirname, "../public/images/" + file.name));
   } catch (err) {
     console.log(err);
