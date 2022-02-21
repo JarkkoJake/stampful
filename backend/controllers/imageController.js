9d641ef8ce31ecd75ef3e9e0503b4517b96d7533
Jarkko Heinonen <71126476+JarkkoJake@users.noreply.github.com>
2022-02-21 11:37:46 +0200

    Saving images
    images saved to public/images, file name gets the image id attached to the end of it to avoid duplicate names.

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,9 +1,22 @@
-//const imageDb = require("../db/image");
+const imageDb = require("../db/image");
+const {join} = require("path");
 
-exports.getImageWithId = (req, res) => {
+exports.getImageWithId = async (req, res) => {
   console.log(req.params.id);
 };
 
-exports.postImage = (req, res) => {
-  console.log("asd");
+exports.postImage = async (req, res) => {
+  try {
+    let newImage = {auctionId: req.params.auctionId};
+    let imageId = await imageDb.postImage(newImage);
+    let file = req.files["thumbnail"];
+    let splitFileName = file.name.split(".");
+    file.name = splitFileName[0] + "_" + imageId[0] + "." + splitFileName[1];
+    newImage.id = imageId[0];
+    newImage.path = "images/" + file.name;
+    await imageDb.addPath(newImage);
+    await file.mv(join(__dirname, "../public/images/" + file.name));
+  } catch (err) {
+    console.log(err);
+  }
 };
