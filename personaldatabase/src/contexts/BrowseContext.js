086a5fc375b7899e82b8276dbdc65751648d48ef
Steve Hommy <steve96hommy@gmail.com>
2022-02-21 13:48:31 +0200

    Browse

--- personaldatabase/src/contexts/BrowseContext.js
+++ personaldatabase/src/contexts/BrowseContext.js
@@ -4,10 +4,28 @@ import { constants } from "../Constants.js";
 export const BrowseContext = createContext();
 
 export const BrowseProvider = ({ children }) => {
+
+  const defaultRequestObj = {listType: "listbrowse", page: 1};
+
+  const [requestObject, setRequestObject] = useState(defaultRequestObj);
+
+  const constructUrl = async () => {
+    const keys = Object.keys(requestObject);
+    let URL = constants.URL + "/auctions";
+    keys.forEach(key => {
+      if (key === "listType") URL = URL + "/" + requestObject.listType + "?";
+      else URL = URL + "&" + key + "=" + requestObject[key];
+    });
+    console.log(URL);
+    return URL;
+  };
+
   return (
     <BrowseContext.Provider
       value={{
-        
+        constructUrl,
+        requestObject,
+        setRequestObject,
       }}
     >
       {children}
