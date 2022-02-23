7400432a15c2910c6df6241953303091f7b5bef3
Steve Hommy <steve96hommy@gmail.com>
2022-02-23 11:58:23 +0200

    Detailed view

--- personaldatabase/src/components/Detailed/Detailed.js
+++ personaldatabase/src/components/Detailed/Detailed.js
@@ -38,19 +38,41 @@ const Detailed = () => {
       <div id="postWrapper">
         <Row justify="space-around" align="middle" className="firstRow">
           <Col className="columnFirstRow" style={{width: "calc(33% - 5px)"}}>
-            <Col
+            <div
               className="categoryMenu"
-              placeholder="Country"
-            >{detailedObject.catalogueNumber ? detailedObject.catalogueNumber : "-"}
-            </Col>
+              placeholder="Country">
+              {detailedObject.country ? detailedObject.country.name : "-"}
+            </div>
+            <div
+              className="categoryMenu"
+              placeholder="Country">
+              {detailedObject.category1 ? detailedObject.category1 : "-"}
+            </div>
+            <div
+              className="categoryMenu"
+              placeholder="Country">
+              {detailedObject.category2 ? detailedObject.category2 : "-"}
+            </div>
+            <div
+              className="categoryMenu"
+              placeholder="Country">
+              {detailedObject.category3 ? detailedObject.category3 : "-"}
+            </div>
           </Col>
+
           <Col className="columnFirstRow" style={{width: "calc(66% - 5px)"}}>
+            <Col className="centeringWrapperDescription" placeholder="Description" id="descriptionField">
+              {detailedObject.description ? detailedObject.description : "-"}
+            </Col>
           </Col>
         </Row>
 
         <Row justify="space-around" align="middle" className="secondRow">
           <Col className="columnThumbnail" style={{width: "calc(40% - 5px)"}}>
             <Col className="thumbnailFirstRow" style={{width: "calc(50% - 5px)"}}> 
+            </Col>
+            <Col className="image">
+              <img className="auctionImage" src={detailedObject.thumbnail ? constants.URL + "/" + detailedObject.thumbnail.path : logo} alt="Logo"></img>
             </Col>
             <Col className="thumbnailSecondRow">
             </Col>
@@ -63,22 +85,33 @@ const Detailed = () => {
               <Col className="stampInfoColumn" style={{width: "calc(100% - 5px)"}}>
 
                 <Row justify="space-around" align="middle" className="stampInfoRowTopTop">
+                  <Col className="checkBoxText">
+                    {detailedObject.used ? detailedObject.used : "-"}
+                  </Col>
+                  <Col className="checkBoxText">
+                    {detailedObject.mint ? detailedObject.mint : "-"}
+                  </Col>
+                  <Col className="checkBoxText">
+                    {detailedObject.postalItem ? "☑" : "-"}
+                  </Col>
+                  <Col className="checkBoxText">
+                    {detailedObject.certificate ? detailedObject.certificate : "-"}
+                  </Col>
                   <Col
-                    id="infoInput"
-                    style={{minWidth: "130px"}}
-                    placeholder="catalogText">{detailedObject.catalogueNumber ? detailedObject.catalogueNumber : "-"}
+                    id="infoInput" style={{minWidth: "130px"}} placeholder="catalogText">
+                    {detailedObject.catalogueNumber ? detailedObject.catalogueNumber : "-"}
                   </Col>
                 </Row>
                 
                 <Row align="middle" className="stampInfoRowTopBottom">
-                  <Col id="infoInputStartingPrice" style={{minWidth: "180px"}} 
-                    placeholder="Starting price" > {detailedObject.startingPrice  ? detailedObject.startingPrice : "-" }{detailedObject.currency ? detailedObject.currency : "" }
+                  <Col id="infoInputStartingPrice" style={{minWidth: "180px"}} placeholder="Starting price">
+                    {detailedObject.startingPrice  ? detailedObject.startingPrice : "-" }{detailedObject.currency ? detailedObject.currency : "" }
                   </Col>
-                  <Col id="infoInputSellingPrice" style={{minWidth: "180px"}} 
-                    placeholder="Selling price" > {detailedObject.sellingPrice ? detailedObject.sellingPrice : "-" }{detailedObject.currency ? detailedObject.currency : "" }
+                  <Col id="infoInputSellingPrice" style={{minWidth: "180px"}} placeholder="Selling price">
+                    {detailedObject.sellingPrice ? detailedObject.sellingPrice : "-" }{detailedObject.currency ? detailedObject.currency : "" }
                   </Col>
-                  <Col id="infoInputCurrency" 
-                    placeholder="Selling price" >{detailedObject.currency ? detailedObject.currency : "" }
+                  <Col id="infoInputCurrency" placeholder="Selling price">
+                    {detailedObject.currency ? detailedObject.currency : "" }
                   </Col>
                 </Row>
 
@@ -87,8 +120,20 @@ const Detailed = () => {
             <Row justify="space-around" align="middle" className="stampSellerRow">
               <Col className="stampSellerColumn" style={{width: "calc(100% - 5px)"}}>
                 <Row justify="space-around" align="middle" className="stampInfoRowBottom">
+                  <Col className="infoInputBottom" placeholder="Selling year">
+                    {detailedObject.sellingYear ? detailedObject.sellingYear : "-"}
+                  </Col>
+                  <Col className="infoInputBottom" placeholder="Auction number">
+                    {detailedObject.auctionNumber ? detailedObject.auctionNumber : "-"}
+                  </Col>
                 </Row>
                 <Row justify="space-around" align="middle" className="stampInfoRowBottom">
+                  <Col className="infoInputBottom" placeholder="Selling year">
+                    {detailedObject.seller ? detailedObject.seller.name : "-"}
+                  </Col>
+                  <Col className="infoInputBottom" placeholder="Selling year">
+                    {detailedObject.lotNumber ? detailedObject.lotNumber : "-"}
+                  </Col>
                 </Row>
               </Col>
             </Row>
