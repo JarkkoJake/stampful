43c443bd56cd6abf53e87397629d018350a708f3
Jarkko Heinonen <71126476+JarkkoJake@users.noreply.github.com>
2022-10-02 21:22:14 +0300

    rest of dropdown migrations made

--- backend/db/migrations/20220929073212_intial_drop_down_tables.js
+++ backend/db/migrations/20220929073212_intial_drop_down_tables.js
@@ -4,10 +4,39 @@ exports.up = function(knex) {
   .createTable("Countries", function (table) {
     table.increments("id");
     table.string("name").notNullable();
-  });
+  })
+  .createTable("Sellers", function (table) {
+    table.increments("id");
+    table.string("name").notNullable();
+  })
+  .createTable("Category1", function (table) {
+    table.increments("id");
+    table.string("category1");
+    table.integer("country").references("id")
+      .inTable("Countries").onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  })
+  .createTable("Category2", function (table) {
+    table.increments("id");
+    table.string("category2");
+    table.integer("category1").references("id")
+      .inTable("Category1").onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  })
+  .createTable("Category3", function (table) {
+    table.increments("id");
+    table.string("category3");
+    table.integer("category2").references("id")
+      .inTable("Category2").onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  })
 };
 
 exports.down = function(knex) {
   return knex.schema
-  .dropTable("Countries");
+  .dropTable("Countries")
+  .dropTable("Sellers")
+  .dropTable("Category1")
+  .dropTable("Category2")
+  .dropTable("Category3")
 };
