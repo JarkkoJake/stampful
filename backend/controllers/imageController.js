81d20df71915bb835343067380e931a1f3b2f541
Jarkko Heinonen <71126476+JarkkoJake@users.noreply.github.com>
2023-10-04 18:22:12 +0300

    work on new postimages controller function, wip

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -38,3 +38,49 @@ exports.postImage = async (req, res) => {
     console.log(err);
   }
 };
+ // WIP
+exports.newPostImage = async (req, res) => {
+  try {
+    const auctionId = req.params.auctionId;
+    if (!auctionId) res.status(400).send("No auction id was provided");
+    const thumbnail = req.files["thumbnail"];
+    if (thumbnail) await setThumbnail(thumbnail, auctionId);
+    const additionalImages = req.files["additionalImages"];
+    if (additionalImages) await saveAdditionalImages(additionalImages, auctionId);
+  } catch (e) {
+    console.log(e)
+  }
+};
+
+const setThumbnail = async (thumbnail, auctionId) => {
+  /* Post thumbnail image to get image id */
+  const thumbnailId = (await imageDb.postImage({auctionId}))[0];
+  const splitFileName = thumbnail.name.split(".");
+  thumbnail.name = `${splitFileName[0]}_${thumbnailId}.${splitFileName[0]}`;
+  const newImageData = {
+    auctionId,
+    id: thumbnailId,
+    path: `images/${thumbnail.name}`,
+  };
+  const auctionUpdate = {
+    id: auctionId,
+    thumbnail: thumbnailId,
+  };
+  await auctionDb.editAuction(auctionUpdate);
+  await imageDb.addPath(newImageData);
+  await thumbnail.mv(join(__dirname, `..public/images/${thumbnail.name}`));
+};
+
+const saveAdditionalImages = async (additionalImages, auctionId) => {
+  for (const image of additionalImages) {
+    const imageId = (await imageDb.postImage({auctionId}))[0];
+    const splitFileName = image.name.split(".");
+    image.name = `${splitFileName[0]}_${imageId}.${splitFileName[1]}`;
+    await imageDb.addPath({
+      auctionId,
+      id: imageId,
+      path: `images/${image.name}`,
+    });
+    await image.mv(join(__dirname, `..public/images/${image.name}`));
+  }
+};
